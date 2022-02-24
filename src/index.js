8707a4f7e45321256d46a5ca5b080306cd7e8f06
David Vandensteen <dvandensteen@gmail.com>
2022-02-24 15:31:11 +0100

    Update : args handler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import params from '#src/lib/params';
+import fs from 'fs-extra';
+import appRootPath from 'app-root-path';
+import Params from '#src/lib/params';
 import MidiKey from '#src/lib/midikeyout';
 
+const { readJSONSync } = fs;
+const { resolve } = appRootPath;
 const { log } = console;
+const { exit } = process;
+
+const params = new Params();
 const midiKey = new MidiKey();
 
-if (params.list) {
-  log(midiKey.getAvailableInterfacesName());
-  process.exit(0);
-} else {
-  midiKey.register({ portId: params.id })
-    .sendCCMessage({ cc: params.cc, value: params.value })
-    .stop();
+if (params.help) Params.help();
+
+if (params.version) {
+  const { version } = readJSONSync(resolve('./package.json'));
+  log(version);
+  exit(0);
 }
+
+if (params.list) {
+  const availableInt = midiKey.getAvailableInterfacesName();
+  const displayInt = [];
+
+  let id = -1;
+  availableInt.map((int) => {
+    id += 1;
+    return displayInt.push(`${id} ${int}`);
+  });
+
+  log(displayInt.join('\n'));
+  exit(0);
+}
+
+if (
+  params.id === undefined
+  || params.cc === undefined
+  || params.value === undefined
+) Params.help();
+
+midiKey.register({ portId: params.id })
+  .sendCCMessage({ cc: params.cc, value: params.value })
+  .stop();
--- src/lib/params.js
+++ src/lib/params.js
@@ -2,28 +2,30 @@ import arg from 'arg';
 
 const { log } = console;
 
-class Params {
+export default class Params {
   constructor() {
     this.args = arg({
       '--id': Number,
       '--cc': Number,
       '--value': Number,
       '--list': Boolean,
+      '--version': Boolean,
+      '--help': Boolean,
 
       // Aliases
       '-i': '--id',
       '-c': '--cc',
       '-v': '--value',
       '-l': '--list',
+      '-h': '--help',
     });
-
     if (process.argv.length <= 1) Params.help();
   }
 
   static help() {
     log('');
     log('');
-    log('CMD', '[options]');
+    log('midi-cmd', '[options]');
     log('');
     log('     Required options:');
     log('');
@@ -34,6 +36,7 @@ class Params {
     log('     Extra options:');
     log('');
     log('   --list     -l               -- show available midi interface');
+    log('   --version                   -- show version');
     log('   --help     -h               -- show help');
     process.exit(0);
   }
@@ -44,19 +47,25 @@ class Params {
 
   get cc() {
     if (this.args['--cc'] < 0) return 0;
-    if (this.args['--cc'] > 128) return 128;
+    if (this.args['--cc'] > 127) return 127;
     return this.args['--cc'];
   }
 
   get value() {
     if (this.args['--value'] < 0) return 0;
-    if (this.args['--value'] > 128) return 128;
+    if (this.args['--value'] > 127) return 127;
     return this.args['--value'];
   }
 
   get list() {
     return this.args['--list'];
   }
-}
 
-export default new Params();
+  get version() {
+    return this.args['--version'];
+  }
+
+  get help() {
+    return this.args['--help'];
+  }
+}
